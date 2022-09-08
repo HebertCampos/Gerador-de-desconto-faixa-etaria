152edc4fdc1c0eaef93dcfea5c16c7548962d5e2
Hebert <hebertcdev@gmail.com>
2022-09-08 13:23:41 -0300

    Ajustando as idades

--- controls/calculo_desconto.py
+++ controls/calculo_desconto.py
@@ -1,2 +1,10 @@
+
 def main_desconto(idades, titular, valCotas):
-    pass
+    if titular == 'No' and idades[0] <= 38:
+        desconto = 0.2
+    else: desconto = 0
+    idade = idades[:]
+    idade.sort(reverse = True)
+    
+    result = zip(idade)
+    return result
